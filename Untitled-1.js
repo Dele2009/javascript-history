b8c93670d265f4ff04fc1c8d2d8314058ea54d75
Dele2009 <obedaminu303@gmail.com>
2023-12-11 15:07:43 +0100

    changes

---
+++ Untitled-1.js
@@ -0,0 +1,167 @@
+
+Types of operator by function they perform
+    arithimetic
+    assignment
+    concatenation
+    comparison
+    logical
+    typescomma
+    bitwise
+    conditionary
+
+    arithimetic operator(they do the work of mathematicalExpression)
+      +, -, *, **, % ,++,--
+         
+         example
+         let a = 6;
+         let b = 4;
+         let c = 
+        //  test later
+
+    concatenation
+
+
+    assignment operator
+    =, +=, -=, *=, /=, %=,
+_____________________________    
+
+       =
+       var a = 'olu';
+
+
+_____________________________
+       +=
+       let x = '2';
+       let y = '3';
+
+       arith
+       x+=y;
+       that means that x new value will be:
+       x=x+y;
+       so x=5
+
+
+    comparison operator
+     == means equal to
+       let a= 3;
+       let b = 6;
+       alert(a==b)
+       answer false (because a is not equal to b)
+_____________________________
+
+     === means that datatype and value are be the same
+     let a= 3;
+     let b = '3';
+     alert(a===b)
+     answer false (because a and b are the same value but not the same datatype)
+
+_____________________________     
+
+     !== means not equal to 
+     let a= 3;
+     let b = '3';
+     alert(a!=b)
+     answer false (because a is equal b in value)
+_____________________________     
+
+     practical example
+       let a= prompt('pls type a number')
+       if(a%2==0)
+       alert('this is an even number')
+    else{
+        alert('this an odd number')
+    }
+
+
+
+    logical operator
+      &&, ||, !
+
+      && logical and
+      || logical or 
+      ! logical not
+
+
+
+
+      note:boolean value are either false or true statements, and empty boolean will give you a false statement  
+
+
+      control flow
+        aka ternary(conditional)operator
+        -if /else statements
+        - switch
+
+        let age =prompt("your age");
+        let beverage=(age>21)?'beer':'water';
+        alert(beverage)
+
+        
+        
+        
+  typeof operator
+
+
+    function
+        calling of function is called function invocation
+        {} uesd to group function or store action
+        () called parameter 
+        taiwo(x, y) is called function parameter
+        taiwo(3, 2) is called fuction argument
+
+
+        ways of writing 
+        function declaration
+        function expression/annonymous-function: let identifier =function(){}
+        arrow fuction: ()=>{}  
+
+
+        D.O.M MANIPULATION
+          document object model
+
+         EXAMPLES
+           document.getElementById('')
+          element.innerHTML
+          element.innerText/Textcontent
+          document.querySelector('')
+          Element.style.property
+          addEventlistener('event',function)
+          document.querySelectorAll('')
+          Element.classlisted.add('class')
+          Element.classlist.remove('class')
+          document.createElement()
+          document.createTextNode()
+          .appendChild()
+          document.getElementsByClassName()
+          ParentNode
+          ChildNode
+          setTimeout()
+          setInterval()
+          element.attribute
+
+
+          use carmel cases for
+
+
+          math method
+          Math.random(),
+
+        
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
